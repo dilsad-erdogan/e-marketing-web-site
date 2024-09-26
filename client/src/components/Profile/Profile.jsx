7e2472aac19d88f5d96e207a6e8eeced1dcd4473
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-09-26 19:58:27 +0300

    login register settings

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import Login from "./Login";
+import Register from "./Register";
+import Settings from "./Settings";
 
 const Profile = () => {
+  const [showsignin, setShowsignin] = useState(false);
+  const id = localStorage.getItem('userId');
+
+  const handleSignIn = () => {
+    setShowsignin(!showsignin);
+  };
+
   return (
-    <div>Profile</div>
+    <div className="container">
+      {id === null ? (
+        <div className="sm:w-[600px] md:w-[380px]">
+          {showsignin ? <Register handleSignIn={handleSignIn} /> : <Login handleSignIn={handleSignIn} />}
+        </div>
+      ) : (
+        <Settings />
+      )}
+    </div>
   )
 }
 
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -2,6 +2,9 @@ import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import Profile from "../components/Profile/Profile";
 
+localStorage.removeItem('user');
+localStorage.removeItem('userId');
+
 const ProfilePage = () => {
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden min-h-screen flex flex-col">
