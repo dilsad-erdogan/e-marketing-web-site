c88cbce75abcd2a598672a2bb3593f0b611fcd57
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-09-30 21:00:58 +0300

    logined

--- client/src/components/Products/ProductsInOrder.jsx
+++ client/src/components/Products/ProductsInOrder.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 
 const ProductsInOrder = () => {
+  const [message, setMessage] = useState(false);
+
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+    if(!user){
+      setMessage(true);
+    }
+  }, []);
+
   return (
-    <div>ProductsInOrder</div>
+    <div className="container">
+      {message ? (
+        <div className="flex justify-center items-center min-h-[50vh]">
+          <h2 className="text-center font-bold text-3xl p-6 border border-gray-300 shadow-lg rounded-lg bg-white">Lütfen önce giriş yapınız!</h2>
+        </div>
+      ) : (
+        <div>
+          <p>Giriş yapıldı</p>
+        </div>
+      )}
+    </div>
   )
 }
 
