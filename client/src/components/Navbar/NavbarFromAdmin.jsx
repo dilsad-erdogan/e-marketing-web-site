fe177163efae6362a1061c5adfa40a5d1a20ba05
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-10-05 21:26:00 +0300

    navbar button add

--- client/src/components/Navbar/NavbarFromAdmin.jsx
+++ client/src/components/Navbar/NavbarFromAdmin.jsx
@@ -1,6 +1,15 @@
 import DarkMode from "./DarkMode";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('profile');
+    navigate('/');
+  };
+  
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
       <div className="py-4">
@@ -13,6 +22,8 @@ const Navbar = () => {
           {/* Search and darkMode section */}
           <div className="flex justify-between items-center gap-4">
             <DarkMode />
+
+            <button className="bg-red-600 text-white cursor-pointer hover:scale-105 duration-300 py-2 px-8 rounded-full relative z-10" onClick={logout}>LogOut</button>
           </div>
         </div>
       </div>
